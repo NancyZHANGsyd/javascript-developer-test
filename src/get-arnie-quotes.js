c36d9f2f4a04da9aa91be41cd66573b2b6225c0e
Nancy Zhang <nancyzhang@nancys-mbp.lan>
2021-06-06 22:37:15 +1000

    Coding Challenge: Implementation of function getArnieQuptes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
 
+const ResultKeyEnum = {
+  SUCCESS: 'Arnie Quote',
+  FAILURE: 'FAILURE',
+};
+
+/**
+ * Get a single Arnie Quote for a provided url
+ * @param url - single url, validation is omit in this case
+ * @returns {Promise<*>}
+ */
+const getArnieQuote = async (url) => {
+  const res = await httpGet(url);
+  const key = res.status === 200 ? ResultKeyEnum.SUCCESS : ResultKeyEnum.FAILURE;
+  return { [key]: JSON.parse(res.body).message };
+};
+
+/**
+ * Get a list of Arnie Quotes for provided urls
+ * @param urls - Array of urls, validation is omit in this case
+ * @returns {Promise<[]>}
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return Promise.all(urls.map(url => getArnieQuote(url)));
 };
 
 module.exports = {
